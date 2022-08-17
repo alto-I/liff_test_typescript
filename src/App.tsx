bbfc1957321e80cb43907c6b3c42057d551edad4
alto-I <blmaldiy@gmail.com>
2022-08-17 15:29:18 +0900

    form作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,109 @@
-import { useEffect, useState } from "react";
-import liff from "@line/liff";
-import "./App.css";
+import axios from 'axios';
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import liff from '@line/liff';
+import { getCsrfToken } from "./getCsrfToken";
 
-function App() {
-  const [message, setMessage] = useState("");
-  const [error, setError] = useState("");
+type reviewTarget = {
+  id: string;
+  name: string;
+};
 
-  useEffect(() => {
-    liff
-      .init({
-        liffId: import.meta.env.VITE_LIFF_ID
-      })
-      .then(() => {
-        setMessage("LIFF init succeeded.");
-      })
-      .catch((e: Error) => {
-        setMessage("LIFF init failed.");
-        setError(`${e}`);
-      });
-  });
+type Props = {
+  reviewTargets: Array<reviewTarget>;
+  userId: string
+};
+
+type Inputs = {
+  product: string;
+  totalScore: number;
+  nickname: string;
+  title: string;
+  description: string;
+};
+
+const LineForm = ({ reviewTargets, userId }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const saveReview = (data: Inputs) => {
+    axios.post(
+      '/line/reviews',
+      {
+        review: {
+          user_id: userId,
+          review_target_id: data.product,
+          total_score: data.totalScore,
+          nickname: data.nickname,
+          from_line: true
+        }
+      },
+      {
+        headers: {
+          'X-CSRF-Token': getCsrfToken()
+        },
+      }
+    ).then((response) => console.log(response))
+    .catch((error) => console.error(error))
+  }
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => alert(JSON.stringify(data))
 
   return (
-    <div className="App">
-      <h1>create-liff-app</h1>
-      {message && <p>{message}</p>}
-      {error && (
-        <p>
-          <code>{error}</code>
-        </p>
-      )}
-      <a
-        href="https://developers.line.biz/ja/docs/liff/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LIFF Documentation
-      </a>
-    </div>
+    <>
+      liff_version: {liff.getVersion()}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='mb-8px'>
+          <label>
+            商品
+            <select className='form-input form-input--block' {...register('product')}>
+              {/* {reviewTargets.map((reviewTarget) => (
+                <option key={reviewTarget.id} value={reviewTarget.id}>
+                  {reviewTarget.name}
+                </option>
+              ))} */}
+              <option value='1'>クレンジング</option>
+              <option value='2'>フォーム</option>
+            </select>
+          </label>
+        </div>
+        <div className='mb-8px'>
+          <label>
+            総合評価
+            <select {...register('totalScore')} defaultValue={'3'}>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+            </select>
+          </label>
+        </div>
+        <div className='mb-8px'>
+          <label>
+            ニックネーム
+            <input {...register('nickname', { required: true })} />
+          </label>
+        </div>
+        <div className='mb-8px'>
+          <label>
+            タイトル
+            <input {...register('title', { required: true })} />
+          </label>
+        </div>
+        <div className='mb-8px'>
+          <label>
+            本文
+            <textarea {...register('description', { required: true })} />
+          </label>
+        </div>
+        <input type='submit' value="投稿する" />
+      </form>
+    </>
   );
-}
+};
 
-export default App;
+export default LineForm;
